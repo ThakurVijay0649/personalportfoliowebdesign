fc1938e8208d0c8f344ed81d1a644f6c1bb32fae
Vijay <thakurvijay0006@gmail.com>
2022-07-16 21:26:34 +0530

    Added

---
+++ js/main.js
@@ -0,0 +1,82 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  navbar.classList.toggle('open-menu')
+  menu.classList.toggle("move")
+};
+window.onscroll = ()=>{
+  navbar.classList.remove('open-menu')
+  menu.classList.remove("move")
+}
+
+//Reviews Swiper
+var swiper = new Swiper(".reviews-content", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//Email js
+function validate() {
+  let name = document.querySelector('.name');
+  let email = document.querySelector('.email');
+  let msg = document.querySelector('.message');
+  let sendBtn = document.querySelector('.send-btn');
+  sendBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (name.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    } else {
+      sendmail(name.value, email.value, msg.value);
+      success();
+    }
+  })
+}
+
+validate();
+
+function sendmail(name, email, msg) {
+  emailjs.send("service_x4uuxew", "template_5n5yj4j", {
+    from_name: email,
+    to_name: name,
+    message: msg,
+  });
+}
+
+function emptyerror() {
+  swal({
+    title: "oh No...!",
+    text: "Fields cannot be empty!",
+    icon: "error",
+  });
+}
+function success() {
+  swal({
+    title: "Email Sent Successfully",
+    text: "We will try to reply in 24 hours",
+    icon: "success",
+  });
+}
+
+//header background change on scroll
+
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+  header.classList.toggle('header-active', window.scrollY > 0)
+})
+
+//scroll top
+let sceollTop = document.querySelector('.scroll-top');
+window.addEventListener('scroll', () => {
+  sceollTop.classList.toggle('scroll-active', window.scrollY >= 400)
+})
+
+
